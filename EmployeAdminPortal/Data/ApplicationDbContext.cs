ae8261b39a776bee49381868cc75c587d3002dce
SavindaKaveesha <kaveeshasavinda98@gmail.com>
2024-11-18 11:00:52 +0530

    Initial Commit

---
+++ EmployeAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EmployeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeAdminPortal.Data
+{
+    public class ApplicationDbContext : DbContext //Inherit
+    {
+        //Constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeAdminPortal/Models/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeAdminPortal.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? phone { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
